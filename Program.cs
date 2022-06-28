f2320270b293eb02f3c552c9f8bb1412541a31ab
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-06-28 14:28:56 +0200

    definite le funzioni

--- Program.cs
+++ Program.cs
@@ -1,1 +1,37 @@
 ﻿
+
+//Functions
+void StampaArray(int[] array)
+{
+    string content = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        content += array[i];
+    }
+    Console.WriteLine(content+="]");
+};
+
+int Quadrato(int numero)
+{
+    return numero * numero;
+}
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+    int[] copiaArray = (int[]) array.Clone();
+    for (int i = 0; i < copiaArray.Length; i++)
+    {
+        copiaArray[i] = Quadrato(array[i]);
+    }
+    return copiaArray;
+};
+
+int sommaElementiArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
